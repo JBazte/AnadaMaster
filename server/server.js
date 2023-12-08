7d0cd96948896b536b57822a58e2bd8c5c6d7778
Elisa Alonso <elisaalonsosa@gmail.com>
2023-12-08 13:53:40 +0100

    Added server directories #2
    -Fixed a couple of errors, now it works properly

---
+++ server/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const mongoose = require("mongoose")
+
+const port = process.env.PORT
+const app = express();
+//app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json());
+
+app.use("/api", require("./routes")) //Lee routes/index.js por defecto; accedemos a todas las funcionalidades de cada tabla
+
+const client = new MongoClient(process.env.DB_URI);
+  
+app.listen(port, ()=>{
+    console.log("Servidor escuchando en el puerto "+ port);
+    
+    const db_uri = process.env.DB_URI
+    mongoose.set('strictQuery', false)
+    mongoose.connect(db_uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(()=>{
+        console.log("Conectando a la BD...")
+    }).catch((err)=>{
+        console.log("Se ha detectado un error: "+ err)
+    })
+
+
+});
+
+module.exports = app
